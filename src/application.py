edd5069603825e3d81234b14b0b899003176d0ab
Manuel Cortez <manuel@manuelcortez.net>
2018-01-24 17:41:44 -0600

    Added basic app info

---
+++ src/application.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+name = "MusicDL"
+version = "0.1"
+author = "Manuel Cortéz"
+authorEmail = "manuel@manuelcortez.net"
+copyright = "Copyright (C) 2018, Manuel Cortez"
+description = name+" Is an application that will allow you to download music."
+url = "https://manuelcortez.net/music_dl"
+#update_url = "https://raw.githubusercontent.com/manuelcortez/socializer/master/update-files/socializer.json"
+# The short name will be used for detecting translation files. See languageHandler for more details.
+short_name = "musicdl"
+translators = []
