eebd86a987bc79f42fdb53e75159e88b4ee764bf
Manuel Cortez <manuel@manuelcortez.net>
2018-02-25 07:15:05 -0600

    Changed play to play_pause for avoiding ambiguous situations

--- src/wxUI/menus.py
+++ src/wxUI/menus.py
@@ -4,7 +4,7 @@ import wx
 class contextMenu(wx.Menu):
 	def __init__(self, *args, **kwargs):
 		super(contextMenu, self).__init__(*args, **kwargs)
-		self.play = wx.MenuItem(self, wx.NewId(), _("Play"))
+		self.play = wx.MenuItem(self, wx.NewId(), _("Play/Pause"))
 		self.Append(self.play)
 		self.download = wx.MenuItem(self, wx.NewId(), _("Download"))
 		self.Append(self.download)
