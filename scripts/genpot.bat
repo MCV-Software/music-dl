a247400b7be49c68ef3bcc59a24695eab08c0377
Manuel Cortez <manuel@manuelcortez.net>
2018-02-27 13:41:47 -0600

    Added localization tools for generating catalogs

---
+++ scripts/genpot.bat
@@ -0,0 +1,3 @@
+@echo off
+echo Generating application translation strings...
+C:\Python27\python.exe pygettext.py  -d musicdl ../src/*.pyw ../src/*.py ../src/*/*.py ../src/*/*.pyw ../src/*/*/*.py ../src/*/*/*.pyw ../src/*/*/*/*.py ../src/*/*/*/*.pyw ../src/*/*/*/*/*.py ../src/*/*/*/*/*.pyw
