1a9200cefeea35e4bc38ea46198f1a6b877fd1f6
Manuel Cortez <manuel@manuelcortez.net>
2018-03-12 12:27:15 -0600

    Added excepthook to logger module

--- src/main.py
+++ src/main.py
@@ -3,12 +3,12 @@ from __future__ import unicode_literals    # at top of module
 import os
 import logging
 import storage
+import traceback
 storage.setup()
 logging.basicConfig(filename=os.path.join(storage.data_directory, "info.log"), level=logging.DEBUG, filemode="w")
+sys.excepthook = lambda x, y, z: logging.critical(''.join(traceback.format_exception(x, y, z)))
 log = logging.getLogger("main")
 log.debug("Logger initialized. Saving debug to {0}".format(storage.data_directory,))
-log.debug("Starting music-dl %s" % (application.version,))
-log.debug("Application path is %s" % (paths.app_path(),))
 import sys
 log.debug("Using Python version {0}".format(sys.version,))
 if sys.version[0] == "2":
@@ -18,11 +18,13 @@ if sys.version[0] == "2":
 		fixes.setup()
 import i18n
 i18n.setup()
-import widgetUtils
 import application
+import widgetUtils
 from platform_utils import paths
 
 def setup():
+	log.debug("Starting music-dl %s" % (application.version,))
+	log.debug("Application path is %s" % (paths.app_path(),))
 	from controller import mainController
 	app = widgetUtils.mainLoopObject()
 	log.debug("Created Application mainloop object")
