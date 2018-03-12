c67c835ecd5fb151cf02127bb219e7476b4aefeb
Manuel Cortez <manuel@manuelcortez.net>
2018-03-12 09:26:37 -0600

    Define Python version in application.py

--- src/application.py
+++ src/application.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
+import sys
+python_version = int(sys.version[0])
 name = "MusicDL"
 version = "0.2"
 author = "Manuel Cortéz"
 authorEmail = "manuel@manuelcortez.net"
 copyright = "Copyright (C) 2018, Manuel Cortez"
-description = name+_(" Is an application that will allow you to download music from popular sites such as youtube, zaycev.net.")
+description = name+_(u" Is an application that will allow you to download music from popular sites such as youtube, zaycev.net.")
 url = "https://manuelcortez.net/music_dl"
 update_url = "https://manuelcortez.net/music_dl/update"
 # The short name will be used for detecting translation files. See languageHandler for more details.
 short_name = "musicdl"
-translators = [_("Manuel Cortez (Spanish)"), ]
+translators = [_(u"Manuel Cortez (Spanish)"), ]
