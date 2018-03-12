c6c0774ec5e7bb98eb88a7e2fd8bc8c8a9cad543
Manuel Cortez <manuel@manuelcortez.net>
2018-03-12 10:02:18 -0600

    Added logger module

--- src/i18n.py
+++ src/i18n.py
@@ -3,11 +3,15 @@ import os
 import gettext
 import locale
 import sys
+import logging
 from platform_utils import paths
+
+log = logging.getLogger("i18n")
 
 def setup():
 	lang = locale.getdefaultlocale()[0]
 	os.environ["lang"] = lang
+	log.debug("System detected language: {0}".format(lang,))
 	if sys.version[0] == "3":
 		gettext.install("musicdl", localedir=os.path.join(paths.app_path(), "locales"))
 	else:
--- src/main.py
+++ src/main.py
@@ -1,23 +1,28 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals    # at top of module
+import os
+import logging
+import storage
+storage.setup()
+logging.basicConfig(filename=os.path.join(storage.data_directory, "info.log"), level=logging.DEBUG, filemode="w")
+log = logging.getLogger("main")
+log.debug("Logger initialized. Saving debug to {0}".format(storage.data_directory,))
+log.debug("Starting music-dl %s" % (application.version,))
+log.debug("Application path is %s" % (paths.app_path(),))
 import sys
+log.debug("Using Python version {0}".format(sys.version,))
 if sys.version[0] == "2":
 	if hasattr(sys, "frozen"):
+		log.debug("Applying fixes for Python 2 frozen executables.")
 		import fixes
 		fixes.setup()
 import i18n
 i18n.setup()
 import widgetUtils
-import logging
 import application
 from platform_utils import paths
 
-logging.basicConfig()
-log = logging.getLogger("main")
-
 def setup():
-	log.debug("Starting music-dl %s" % (application.version,))
-	log.debug("Application path is %s" % (paths.app_path(),))
 	from controller import mainController
 	app = widgetUtils.mainLoopObject()
 	log.debug("Created Application mainloop object")
