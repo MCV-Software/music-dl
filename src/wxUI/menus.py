500671d2a732ba8dfe74f64659d0bb1971a8a84c
Manuel Cortez <manuel@manuelcortez.net>
2018-03-12 09:24:27 -0600

    Added changes to make GUI compatible for Python 2 and 3

--- src/wxUI/menus.py
+++ src/wxUI/menus.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 import wx
+import application
 
 class contextMenu(wx.Menu):
 	def __init__(self, *args, **kwargs):
 		super(contextMenu, self).__init__(*args, **kwargs)
-		self.play = wx.MenuItem(self, wx.NewId(), _("Play/Pause"))
-		self.Append(self.play)
-		self.download = wx.MenuItem(self, wx.NewId(), _("Download"))
-		self.Append(self.download)
+		self.play = wx.MenuItem(self, wx.NewId(), _(u"Play/Pause"))
+		self.download = wx.MenuItem(self, wx.NewId(), _(u"Download"))
+		if application.python_version == 3:
+			self.Append(self.play)
+			self.Append(self.download)
+		else:
+			self.AppendItem(self.play)
+			self.AppendItem(self.download)
