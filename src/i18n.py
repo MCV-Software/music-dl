39994be579bfc147922283aac3067b6859eaa264
Manuel Cortez <manuel@manuelcortez.net>
2018-03-12 09:27:03 -0600

    Install unicode gettext function if Python 2

--- src/i18n.py
+++ src/i18n.py
@@ -2,9 +2,13 @@
 import os
 import gettext
 import locale
+import sys
 from platform_utils import paths
 
 def setup():
 	lang = locale.getdefaultlocale()[0]
 	os.environ["lang"] = lang
-	gettext.install("musicdl", localedir=os.path.join(paths.app_path(), "locales"))
+	if sys.version[0] == "3":
+		gettext.install("musicdl", localedir=os.path.join(paths.app_path(), "locales"))
+	else:
+		gettext.install("musicdl", localedir=os.path.join(paths.app_path(), "locales"), unicode=True)
