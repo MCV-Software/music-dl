9aa61b1e7b38560eebc4947cbbb4b58bea9d9111
Manuel Cortez <manuel@manuelcortez.net>
2018-12-30 01:11:15 -0600

    Fixed requests after running some tests

--- src/fixes/fix_requests.py
+++ src/fixes/fix_requests.py
@@ -8,5 +8,5 @@ log = logging.getLogger("fixes.fix_requests")
 
 def fix():
 	log.debug("Applying fix for requests...")
-	os.environ["REQUESTS_CA_BUNDLE"] = os.path.join(paths.app_path(), "cacert.pem")
+	os.environ["REQUESTS_CA_BUNDLE"] = os.path.join(paths.app_path(), "cacerts.txt")
 	log.debug("Changed CA path to %s" % (os.environ["REQUESTS_CA_BUNDLE"],))
