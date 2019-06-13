dd23ce6adfdd0a118925502a6156e791901e9d60
Manuel Cortez <manuel@manuelcortez.net>
2019-06-12 22:42:51 -0500

    fixed conditional import with  testing suite

--- src/extractors/__init__.py
+++ src/extractors/__init__.py
@@ -3,5 +3,5 @@
 import config
 from . import mailru, youtube, zaycev
 # conditional imports
-if config.app["services"]["tidal"]["username"] != "" and config.app["services"]["tidal"]["password"] != "":
+if config.app != None and config.app["services"]["tidal"]["username"] != "" and config.app["services"]["tidal"]["password"] != "":
 	from . import tidal
