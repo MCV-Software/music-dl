1051b81983642f1204c3934ea94b945a4de13ae0
Manuel Cortez <manuel@manuelcortez.net>
2020-07-17 17:51:02 -0500

    Added alpha version generation for Music DL and autoupdates

--- src/application.py
+++ src/application.py
@@ -8,10 +8,12 @@ authorEmail = "manuel@manuelcortez.net"
 copyright = "Copyright (C) 2019-2020, Manuel Cortez"
 description = name+_(u" Is an application that will allow you to download music from popular sites such as youtube, zaycev.net.")
 url = "https://manuelcortez.net/music_dl"
-update_url = "https://manuelcortez.net/music_dl/update"
 # The short name will be used for detecting translation files. See languageHandler for more details.
 short_name = "musicdl"
 translators = [_(u"Manuel Cortez (Spanish)")]
 bts_name = "music_dl"
 bts_access_token = "fe3j2ijirvevv9"
 bts_url = "https://issues.manuelcortez.net"
+update_stable_url = "https://manuelcortez.net/static/files/music_dl/update/stable.json"
+update_next_url = "https://manuelcortez.net/static/files/music_dl/update/alpha.json"
+update_next_version=None
