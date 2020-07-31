ad2189039e303d8a50348fc5f20fdb2fb9b847a6
Manuel Cortez <manuel@manuelcortez.net>
2020-07-31 10:52:53 -0500

    fixed python checking code no longer valid

--- src/wxUI/menus.py
+++ src/wxUI/menus.py
@@ -1,15 +1,10 @@
 # -*- coding: utf-8 -*-
 import wx
-import application
 
 class contextMenu(wx.Menu):
 	def __init__(self, *args, **kwargs):
 		super(contextMenu, self).__init__(*args, **kwargs)
 		self.play = wx.MenuItem(self, wx.NewId(), _(u"Play/Pause"))
 		self.download = wx.MenuItem(self, wx.NewId(), _(u"Download"))
-		if application.python_version == 3:
-			self.Append(self.play)
-			self.Append(self.download)
-		else:
-			self.AppendItem(self.play)
-			self.AppendItem(self.download)
+		self.Append(self.play)
+		self.Append(self.download)
