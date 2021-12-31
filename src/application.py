32fb52f941e61918d02c7d9bef8ea536fadcfd00
Manuel Cortez <manuel@manuelcortez.net>
2021-12-30 18:49:01 -0600

    Updated copyright sections

--- src/application.py
+++ src/application.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 name = "MusicDL"
-author = "Manuel Cortéz"
-authorEmail = "manuel@manuelcortez.net"
-copyright = "Copyright (C) 2019-2020, Manuel Cortez"
+author = "MCV Software"
+authorEmail = "info@mcvsoftware.com"
+copyright = "Copyright (C) 2019-2022, MCV Software"
 description = name+_(u" Is an application that will allow you to download music from popular sites such as youtube, zaycev.net.")
-url = "https://manuelcortez.net/music_dl"
+url = "https://mcvsoftware.com/music_dl"
 # The short name will be used for detecting translation files. See languageHandler for more details.
 short_name = "musicdl"
 translators = [_(u"Manuel Cortez (Spanish)")]
