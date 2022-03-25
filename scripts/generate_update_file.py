6eaa0f50a1bc4eb68b9b2058bd9fb9bcec6da2be
Manuel Cortez <manuel@manuelcortez.net>
2022-03-25 12:58:13 -0600

    Generate update files with right version

--- scripts/generate_update_file.py
+++ scripts/generate_update_file.py
@@ -1,32 +1,21 @@
 #! /usr/bin/env python
 import os
 import json
+import datetime
 
 print("Generating update files for Socializer...")# Determine if we are going to write stable or alpha update file.
-# Stable file is when we build tags and alpha otherwise.
-version = os.environ.get("CI_COMMIT_TAG") or os.environ.get("CI_COMMIT_SHORT_SHA")
-if os.environ.get("CI_COMMIT_TAG") == None:
-	version_type = "latest"
-else:
-	version_type = "stable"
+version = datetime.datetime.now().strftime("%Y.%m.%D")
+version_type = "latest"
 print("Version detected: %s" % (version_type,))
 
 # Read update description and URL'S
-if version_type == "latest":
-	description = os.environ.get("CI_COMMIT_MESSAGE")
-	urls = dict(Windows32="https://files.mcvsoftware.com/music_dl/latest/music_dl_x86.zip", Windows64="https://files.mcvsoftware.com/music_dl/latest/music_dl_x64.zip")
-else:
-	with open("update-description",'r') as f:
-		description = f.read()
-	urls=dict(Windows32="https://manuelcortez.net/static/files/{v}/music_dl_{v}.zip".format(v=version[1:]), Windows64="http://socializer.su/static/files/{v}/socializer_{v}_x64.zip".format(v=version[1:]))
+description = os.environ.get("CI_COMMIT_MESSAGE")
+urls = dict(Windows32="https://files.mcvsoftware.com/music_dl/latest/music_dl_x86.zip", Windows64="https://files.mcvsoftware.com/music_dl/latest/music_dl_x64.zip")
 
 # build the main dict object
 data = dict(current_version=version, description=description, downloads=urls)
 print("Generating file with the following arguments: %r" % (data,))
-if version_type == "latest":
-	updatefile = "latest.json"
-else:
-	updatefile = "stable.json"
+updatefile = "latest.json"
 f = open(updatefile, "w")
 json.dump(data, f, ensure_ascii=False)
 f.close()
